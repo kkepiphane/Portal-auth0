1035e0d62a4ec8839b97e9182cf9b286d8f50658
kkepiphane <ekoutsava@gmail.com>
2023-10-25 03:49:44 +0000

    Ajoutez des fichiers projet.

---
+++ Portal-auth0/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Portal_auth0.Models;
+
+namespace Portal_auth0.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task Login(string returnUrl = "/")
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+
+        public async Task Signup(string returnUrl = "/")
+        {
+            var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+                .WithParameter("screen_hint", "signup")
+                .WithRedirectUri(returnUrl)
+                .Build();
+
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            var userProfil = new User
+            {
+                UserName = User.Identity.Name,
+                EmailAddress = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value,
+                ProfileImage = User.Claims.FirstOrDefault(c => c.Type == "picture")?.Value
+            };
+            return View(userProfil);
+        }
+
+        [Authorize]
+        public async Task Logout()
+        {
+            var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+                .WithRedirectUri(Url.Action("Index", "Home"))
+                .Build();
+
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        public IActionResult EmailVerification()
+        {
+            return View();
+        }
+    }
+}
---
+++ Portal-auth0/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Portal_auth0.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+        public string ProfileImage { get; set; }
+
+    }
+}
